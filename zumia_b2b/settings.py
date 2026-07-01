952ac663665807c352e7f983efee284c526b368a
tapaskumarsahoo1 <tapaskumarsahoo9090@gmail.com>
2026-07-01 16:51:33 +0530

    ok

--- zumia_b2b/settings.py
+++ zumia_b2b/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-1$rii2$*j^rgfhyxb-c!t0-o62r%p0l$h59k#cw)+gfz)&ufoh'/
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
@@ -179,12 +179,8 @@ CHANNEL_LAYERS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    # 'https://storebg.lightcircle.in',
-    'https://zumiab2b.zumiahomes.com',
-
-
+    'https://zumiab2b.zumiahomes.com'
 ]
 CSRF_TRUSTED_ORIGINS = [
-        # 'https://storebg.lightcircle.in'
         'https://zumiab2b.zumiahomes.com'
 ]
