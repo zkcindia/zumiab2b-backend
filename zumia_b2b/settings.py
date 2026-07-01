3e951282ebb06bd59f6f0f760a137c12110bd78a
tapaskumarsahoo1 <tapaskumarsahoo9090@gmail.com>
2026-07-01 14:43:41 +0530

    fix the asgi.py

--- zumia_b2b/settings.py
+++ zumia_b2b/settings.py
@@ -22,14 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1$rii2$*j^rgfhyxb-c!t0-o62r%p0l$h59k#cw)+gfz)&ufoh'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = config('DEBUG', cast=bool)
 
-# ALLOWED_HOSTS = ['indotaskbg.indotask.com']/
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
